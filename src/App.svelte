13352b7850a7be6c08ed7ce2e0b9d0f9b21a59c6
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2021-11-23 18:05:55 -0500

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Hero from './components/Hero.svelte'
+
+	let primaryColor = '#0000FF'
+	const generatePalettes = (newColor) => {
+		alert('hello from the dispatched function')
+		color = newColor
+	}
+</script>
+
+<main>
+	<Hero 
+		on:handlePaletteClick={generatePalettes}
+		mainColor={primaryColor}/>
+</main>
+
+<style>
+	
+</style>
---
+++ src/components/Hero.svelte
@@ -0,0 +1,60 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+    function handlePaletteClick() {
+        dispatch('handlePaletteClick')
+    }
+
+    export let mainColor
+</script> 
+
+<main id='hero-container'>
+    <nav>
+        <img id='navbar-logo' src='./static/images/color-space-logo.png' alt='The colorspace logo'/>
+        <p id='navbar-styled-text'>ColorSpace</p>
+        <ul>
+            <li>Palettes</li>
+            <li>Gradient</li>
+            <li>3-Color-Gradient</li>
+            <li>Contact</li>
+            <li>Info</li>
+            <img id='navbar-twitter-logo' src='...' alt='The twitter logo'/>
+        </ul>
+    </nav>
+    <button 
+        on:click={handlePaletteClick}
+        style='background: {mainColor}'>generate a palette</button>
+</main>
+
+<style>
+    main {
+        height: 100vh;
+        width: 100vw;
+        background: seagreen;
+    }
+    nav {
+        border: 1px solid red;
+        width: 100%;
+        height: 5vmax;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    #navbar-logo {
+        height: 2vmax;
+        margin-left: 10%;
+    }
+    #navbar-styled-text {
+        font-weight: 700;
+    }
+    ul {
+        margin-left: auto;
+        margin-right: 10%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 2vmax;
+    }
+
+</style>
